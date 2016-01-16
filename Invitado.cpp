9901b0f98e5b4ddcc75bad8f67e62775da5ea07e
IsaacRocos <angelrc@live.com.mx>
2016-01-16 14:30:22 -0600

    Invitado terminado

--- Invitado.cpp
+++ Invitado.cpp
@@ -1,5 +1,8 @@
+/*
+* Invitado. Se ejecuta cuando el usuario desea unirse a otro para chatear.
+*/
+
 #include "h/Chat.h"
-#include "h/Mensaje.h"
 
 
 int init_Socket(int *chat_Socket){
@@ -48,10 +51,11 @@ int cerrarConexion(int *chat_Socket){
 }
 
 
-int main(int argv, char** argc){
+int main(int argc, char** argv){
 
 	int host_port= 1101;
     char* host_name="127.0.0.1";
+    //char* host_name="192.168.43.28";
     struct sockaddr_in direccion;
     pthread_t thread_id=0;
     pthread_t thread_id1=0;
@@ -64,14 +68,27 @@ int main(int argv, char** argc){
     int tamanioMSG;
     char *msjSerializado;
 
+    if(argc==2){
+        host_name = argv[1];
+        setMostrarFlujo(0);
+    }else if(argc ==3){
+        host_name = argv[1];
+        setMostrarFlujo(atoi(argv[2]));
+    }else{
+        cout << "Usando dirección local"<<endl;
+        setMostrarFlujo(0);
+    }
+
     string usuario;
-    cout<<"Nombre de Usuario: ";
+    cout<<">> Nombre de Usuario: ";
     getline(cin,usuario);
     setUsuario(usuario);
     setTUsuario(2);
 
+    cout<<"Conectando a: "<<host_name<<endl;
+
     init_Socket(&chat_Socket);
-	  conectar_Chat(p_int,&chat_Socket, &direccion,&host_port,host_name);    
+	conectar_Chat(p_int,&chat_Socket, &direccion,&host_port,host_name);    
     pthread_create(&thread_id,0,&conversar, (void*)&chat_Socket);
     pthread_create(&thread_id1,0,&SocketHandler, (void*)&chat_Socket);
     void* estado;
